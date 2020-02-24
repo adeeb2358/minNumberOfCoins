cde0406943cfa26ca5f91095ad66d92b463bb8ac
Adeeb Mohammed <adeeb2358@gmail.com>
2020-02-25 01:05:42 +0900

    Initial Commit

---
+++ minNumberOfCoins/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  minNumberOfCoins
+//
+//  Created by adeeb mohammed on 25/02/20.
+//  Copyright © 2020 adeeb mohammed. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
